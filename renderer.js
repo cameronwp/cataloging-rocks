42d6181a0bf47f5c14e42e4101fbc502d4dc4330
cameronwp <cameron.w.pittman@gmail.com>
2016-06-21 11:08:10 -0600

    adds dimension display

--- renderer.js
+++ renderer.js
@@ -5,6 +5,13 @@ const stateManager = new StateManager();
 // stateManager.clear();
 var currentDimensions = document.querySelector('.current-dimensions');
 var dimensionToAdd = document.querySelector('input#add-dimension');
+var dimensionDisplay = document.querySelector('#dimension-display');
+var editDimensionsToggle = document.querySelector('#edit');
+var editDimensions = document.querySelector('.edit-dimensions');
+
+editDimensionsToggle.onclick = () => {
+  editDimensions.classList.toggle('hide');
+};
 
 function createDimensionEdit(dimension) {
   const container = document.createElement('div');
@@ -15,6 +22,7 @@ function createDimensionEdit(dimension) {
   name.innerHTML = 'Dimension: ' + dimension.name;
 
   const removeDimension = document.createElement('button');
+  removeDimension.classList.add('left');
   removeDimension.innerText = 'X';
 
   removeDimension.onclick = function(e) {
@@ -34,6 +42,7 @@ function createDimensionEdit(dimension) {
     category.innerHTML = 'Category: ' + cName;
 
     const removeCategory = document.createElement('button');
+    removeCategory.classList.add('left');
     removeCategory.innerText = 'X';
     removeCategory.onclick = function(e) {
       var category = cName;
@@ -74,8 +83,15 @@ function refreshDisplay() {
   if (stateManager.getDimensions().length > 0) {
     const dimensions = stateManager.getDimensions();
     currentDimensions.innerHTML = '';
-    dimensions.forEach(dimension => currentDimensions.appendChild(createDimensionEdit(dimension)));
+    dimensionDisplay.innerHTML = '';
+    dimensions.forEach(dimension => {
+      currentDimensions.appendChild(createDimensionEdit(dimension))
+      let tr = document.createElement('tr');
+      tr.innerHTML = dimension.name + '<br>' + dimension.categories.map(c => '<em>' + c + '</em>').join('<br>');
+      dimensionDisplay.appendChild(tr);
+    });
   } else {
+    dimensionDisplay.innerHTML = 'none';
     currentDimensions.innerHTML = 'No dimensions yet';
   }
 };
