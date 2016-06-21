4822bc7587a6d4395924a90cde9888b59feb8e94
cameronwp <cameron.w.pittman@gmail.com>
2016-06-21 10:02:37 -0600

    init state manager (for dimensions)

--- renderer.js
+++ renderer.js
@@ -1,3 +1,52 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const csv = require('csv');
+const StateManager = require('./state-manager');
+
+const stateManager = new StateManager();
+// stateManager.clear();
+var currentDimensions = document.querySelector('.current-dimensions');
+var removeDimensionsDatalist = document.querySelector('#dimension-list');
+var dimensionToAdd = document.querySelector('input#add-dimension');
+var dimensionToRemove = document.querySelector('#dimension-to-remove');
+
+function createDimensionEdit() {
+  // needs a name
+}
+
+function refreshDisplay() {
+  dimensionToAdd.value = '';
+
+  removeDimensionsDatalist.innerHTML = '';
+  dimensionToRemove.value = '';
+
+  stateManager.getDimensions().forEach(val => {
+    var o = document.createElement('option');
+    o.for = "remove-dimension";
+    o.value = val.name;
+    removeDimensionsDatalist.appendChild(o);
+  });
+  if (stateManager.getDimensions().length > 0) {
+    const dimensions = stateManager.getDimensions();
+    const names = dimensions.map(name => name.name);
+    currentDimensions.innerHTML = names.join(', ');
+  } else {
+    currentDimensions.innerHTML = 'No dimensions yet';
+  }
+};
+
+refreshDisplay();
+
+dimensionToAdd.onkeyup = function(e) {
+  if (e.keyCode === 13) {
+    var dimension = dimensionToAdd.value;
+    stateManager.addDimension(dimension);
+    refreshDisplay();
+  }
+};
+
+dimensionToRemove.onkeyup = function(e) {
+  if (e.keyCode === 13) {
+    var dimension = dimensionToRemove.value;
+    stateManager.removeDimension(dimension);
+    refreshDisplay();
+  }
+};
---
+++ state-manager.js
@@ -0,0 +1,61 @@
+const remote = require('electron').remote;
+const app = remote.app;
+const fs = require('fs');
+
+var tempFile = null;
+if (!process.env.testing) {
+  tempFile = app.getPath('appData') + '/temp.json';
+} else {
+  tempFile = app.getPath('temp') + '/temp.json';
+}
+
+const nconf = require('nconf').file({file: tempFile});
+
+nconf.load();
+if (!nconf.get('dimensions')) { nconf.set('dimensions', []); }
+if (!nconf.get('inputs')) { nconf.set('inputs', []); }
+
+function StateManager() {
+  const self = this;
+  function findDimensionIndex(dimension) {
+    if (!dimension) { throw new TypeError(); }
+    return self.getDimensions().findIndex(d => d.name === dimension);
+  }
+
+  this.getDimensions = () => {
+    return nconf.get('dimensions');
+  }
+
+  this.addDimension = dimension => {
+    const foundIndex = findDimensionIndex(dimension);
+    if (foundIndex === -1) {
+      nconf.get('dimensions').push({name: dimension, categories: []});
+      nconf.save();
+    }
+  };
+
+  this.removeDimension = dimension => {
+    const foundIndex = findDimensionIndex(dimension);
+    if (foundIndex > -1) {
+      nconf.get('dimensions').splice(foundIndex, 1);
+      nconf.save();
+    }
+  };
+
+  this.addCategory = (dimension, category) => {
+    const foundIndex = findDimensionIndex();
+
+    if (foundIndex > -1) {
+      const dimension = nconf.get('dimensions')[foundIndex];
+      
+    }
+  };
+
+  this.clear = () => {
+    nconf.set('dimensions', []);
+    nconf.set('inputs', []);
+    nconf.save();
+  };
+};
+
+module.exports = StateManager;
