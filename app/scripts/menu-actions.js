b8d8008552456fb753b48c298b43081450ee877c
cameronwp <cameron.w.pittman@gmail.com>
2016-06-26 15:52:26 -0600

    adds menu actions for later
    Update README.md

---
+++ app/scripts/menu-actions.js
@@ -0,0 +1,28 @@
+// const {remote} = require('electron');
+// const {Menu, MenuItem} = remote;
+
+// const template = [
+//   {
+//     label: 'Edit',
+//     submenu: [
+//       {
+//         label: 'Save',
+//         accelerator: 'CmdOrCtrl+S',
+//         role: 'save'
+//       },
+//       {
+//         label: 'Open',
+//         accelerator: 'CmdOrCtrl+O',
+//         role: 'open'
+//       },
+//       {
+//         label: 'Refresh',
+//         accelerator: 'CmdOrCtrl+R',
+//         role: 'refresh'
+//       }
+//     ]
+//   }
+// ];
+
+// const menu = Menu.buildFromTemplate(template);
+// Menu.setApplicationMenu(menu);
