48fed403eb8bccfdec094f3596ba0e3e92c8f3e6
cameronwp <cameron.w.pittman@gmail.com>
2016-06-26 16:05:54 -0600

    finishing touches for release

--- main.js
+++ main.js
@@ -10,13 +10,15 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize;
+
+  mainWindow = new BrowserWindow({width: width, height: height})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/app/index_polymer.html`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
